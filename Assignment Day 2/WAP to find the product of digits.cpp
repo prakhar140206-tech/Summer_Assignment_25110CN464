5a9c10830e7e3d134324341be905632d5d97e442
prakhar140206-tech <prakhar140206@gmail.com>
2026-06-05 10:27:25 +0530

    Add files via upload

---
+++ Assignment Day 2/WAP to find the product of digits.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num, product = 1;
+
+    cout << "Enter a number: ";
+    cin >> num;
+
+    while (num > 0) {
+        product *= (num % 10);
+        num /= 10;
+    }
+
+    cout << "Product of digits = " << product;
+
+    return 0;
+}
