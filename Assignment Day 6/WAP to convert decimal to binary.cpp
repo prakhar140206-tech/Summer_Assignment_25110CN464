64f168b993880939e01de67d5ff7319a757e985d
prakhar140206-tech <prakhar140206@gmail.com>
2026-06-09 15:10:44 +0530

    Add files via upload

---
+++ Assignment Day 6/WAP to convert decimal to binary.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    string binary = "";
+
+    while(n > 0) {
+        binary = char((n % 2) + '0') + binary;
+        n /= 2;
+    }
+
+    cout << binary;
+
+    return 0;
+}
